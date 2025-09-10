1a08e4083cf877af9ee89c6b5ef39d45681c57e2
sangamkg11 <sangamkg11@gmail.com>
2025-09-11 02:48:29 +0530

    icon change

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,16 @@ import React from "react";
 function Logo({ width = "100px" }) {
   return (
     <>
-      <div>Logo</div>
+      <div>
+        <img
+          src="../public/icons.png"
+          alt=" LOGO"
+          srcset=""
+          height={50}
+          width={70}
+          className="mt- pt-0"
+        />
+      </div>
     </>
   );
 }
