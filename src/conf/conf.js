d4c8380379837c48210af07ee2df178ca89a2267
sangamkg11 <sangamkg11@gmail.com>
2025-09-11 00:50:29 +0530

    ss

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,6 +4,7 @@ const conf = {
   appwriteDbId: String(import.meta.env.VITE_APPWRITE_DB_ID),
   appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
   appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+  tinymiceAPIKey: String(import.meta.env.VITE_TINYMCE_API_KEY),
 };
 
 export default conf;
